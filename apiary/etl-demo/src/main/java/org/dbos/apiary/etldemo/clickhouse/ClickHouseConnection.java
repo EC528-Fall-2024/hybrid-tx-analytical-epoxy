ea39372dcd49a990066134706dad0d72eea71da2
howeyour <jli3469@bu.edu>
2024-12-05 18:08:51 -0500

    Update ClickHouseConnection.java

--- apiary/etl-demo/src/main/java/org/dbos/apiary/etldemo/clickhouse/ClickHouseConnection.java
+++ apiary/etl-demo/src/main/java/org/dbos/apiary/etldemo/clickhouse/ClickHouseConnection.java
@@ -5,12 +5,36 @@ import ru.yandex.clickhouse.ClickHouseDataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import com.clickhouse.jdbc.ClickHouseDataSource;
+
 public class ClickHouseConnection {
 
-    private static final String CLICKHOUSE_URL = "jdbc:clickhouse://localhost:8123/default"; // Update URL if needed
+    private String clickhouseUrl = "jdbc:clickhouse://localhost:8123/default"; // Default URL
+
+    public ClickHouseConnection() {
+        // Default constructor with default URL
+    }
+
+    public ClickHouseConnection(String clickhouseUrl) {
+        // Constructor to set custom URL
+        this.clickhouseUrl = clickhouseUrl;
+    }
+
+    public void setClickHouseUrl(String clickhouseUrl) {
+        // Setter to update URL dynamically
+        this.clickhouseUrl = clickhouseUrl;
+    }
+
+    public String getClickHouseUrl() {
+        // Getter to retrieve current URL
+        return this.clickhouseUrl;
+    }
 
     public Connection getClickHouseConnection() throws SQLException {
-        ClickHouseDataSource dataSource = new ClickHouseDataSource(CLICKHOUSE_URL);
+        ClickHouseDataSource dataSource = new ClickHouseDataSource(clickhouseUrl);
         return dataSource.getConnection();
     }
+}
 }
