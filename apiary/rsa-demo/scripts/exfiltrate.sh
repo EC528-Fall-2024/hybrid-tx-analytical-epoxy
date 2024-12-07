fc398a84a58d6edb654b6f6a0c34a1aa49607816
davcurse <davidli9009@gmail.com>
2024-12-06 19:02:28 -0500

    clean up of repo

--- apiary/rsa-demo/scripts/exfiltrate.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar target/demo-exec-fat-exec.jar -s exfiltratePosts -username "$1"
--- apiary/scripts/init_mysql.sql
+++
@@ -1,4 +0,0 @@
--- MySQL database name and table name are case sensitive in Unix/Linux!
--- But column names are not.
-CREATE DATABASE IF NOT EXISTS dbos;
-USE dbos;
