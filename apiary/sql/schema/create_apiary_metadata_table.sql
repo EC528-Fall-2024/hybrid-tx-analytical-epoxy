3515d02df9d0567035b8165f019737ecb3a97c35
davcurse <davidli9009@gmail.com>
2024-12-06 19:10:52 -0500

    clean up part 2

--- apiary/sql/schema/create_apiary_metadata_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE ApiaryMetadata (
-             PKEY INTEGER NOT NULL,
-             MetadataKey VARCHAR(1024) NOT NULL,
-             MetadataValue INTEGER,
-             PRIMARY KEY (PKEY, MetadataKey)
-);
-PARTITION TABLE ApiaryMetadata ON COLUMN PKEY;
